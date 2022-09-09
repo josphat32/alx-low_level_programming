214a2ca71ddc02ac9a38a4752e65e5b2cc156113
Josphat32 <mwangijosphat2030@gmail.com>
2022-09-09 15:13:59 -0700

    3 combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,9 +17,12 @@ int main(void)
 				if (l > m && m > n)
 				{
 					putchar(n);
-					putchar(m);															putchar(l);															if (n != 55 || m != 56)
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
 					{
-						putchar(',');															putchar(' ');
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
