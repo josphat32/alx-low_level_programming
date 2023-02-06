1e920add8ffd9816e70c62770cb7f33b21c5cac1
Josphat32 <mwangijosphat2030@gmail.com>
2023-02-06 05:33:18 -0800

    dynamic lib script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
+#gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
+gcc -fPIC -c *.c
 gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
